c75324613e4574477c04daf8adf20aa2d7e261a2
jessieaqc <jessieaqc@email.com>
2026-05-16 22:25:41 -0600

    Se crea un bucket en caso de que no exista para ingest.py

--- pipeline/ingest.py
+++ pipeline/ingest.py
@@ -3,9 +3,19 @@ import urllib.request
 import os
 
 BUCKET = "data-source-52143"
+REGION = "us-east-1"
 
-s3 = boto3.client("s3")
+s3 = boto3.client("s3", region_name=REGION)
 
+# crear bucket si no existe
+try:
+    s3.head_bucket(Bucket=BUCKET)
+    print(f"Bucket '{BUCKET}' ya existe")
+except:
+    s3.create_bucket(Bucket=BUCKET)
+    print(f"Bucket '{BUCKET}' creado")
+
+# archivos de internet
 ARCHIVOS_WEB = {
     "SAU-GLOBAL-1-v48-0.csv": "https://aws-tc-largeobjects.s3.us-west-2.amazonaws.com/CUR-TF-200-ACDENG-1-91570/lab-capstone/s3/SAU-GLOBAL-1-v48-0.csv",
     "SAU-HighSeas-71-v48-0.csv": "https://aws-tc-largeobjects.s3.us-west-2.amazonaws.com/CUR-TF-200-ACDENG-1-91570/lab-capstone/s3/SAU-HighSeas-71-v48-0.csv",
@@ -19,6 +29,7 @@ for nombre, url in ARCHIVOS_WEB.items():
         s3.upload_fileobj(response, BUCKET, destino)
     print(f"Listo")
 
+# archivos de local
 ARCHIVOS_LOCAL = {
     "datasets/SAU EEZ 848 v50-1.csv": "raw/SAU_EEZ_848_v50-1.csv",
 }
